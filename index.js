a48a01912aad83ef44360373e37f13bf1c6a80f7
Aaron Nash <aaron27nash@gmail.com>
2023-07-08 09:57:35 -0400

    Adding 40 page

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ app.get('/', (req, res) => {
 })
 
 app.listen(3000)
+
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+
+app.get('*', (req, res) => {
+    res.send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
+
